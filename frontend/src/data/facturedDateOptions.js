9420d7ede141efb76c1bf093ebcbe389dc14e96d
Santiago Jojoa <santiago.jojoan@gmail.com>
2025-10-14 17:01:58 -0500

    new: -Create dates array for mapping

---
+++ frontend/src/data/facturedDateOptions.js
@@ -0,0 +1,26 @@
+export const monthOptions = [
+    { value: 1, label: '01-Enero' },
+    { value: 2, label: '02-Febrero' },
+    { value: 3, label: '03-Marzo' },
+    { value: 4, label: '04-Abril' },
+    { value: 5, label: '05-Mayo' },
+    { value: 6, label: '06-Junio' },
+    { value: 7, label: '07-Julio' },
+    { value: 8, label: '08-Agosto' },
+    { value: 9, label: '09-Septiembre' },
+    { value: 10, label: '10-Octubre' },
+    { value: 11, label: '11-Noviembre' },
+    { value: 12, label: '12-Diciembre' },
+]
+
+export const yearsOptions = [
+    { value: 2020, label: '2020' },
+    { value: 2021, label: '2021' },
+    { value: 2022, label: '2022' },
+    { value: 2023, label: '2023' },
+    { value: 2024, label: '2024' },
+    { value: 2025, label: '2025' },
+    { value: 2026, label: '2026' },
+    { value: 2027, label: '2027' },
+    { value: 2028, label: '2028' },
+]
